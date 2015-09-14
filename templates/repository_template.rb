aed6b2a255291e8fa5ff70541bbac37c477ea983
Pedro Ramirez <pedro.rmrz.srz@gmail.com>
2015-09-14 16:04:45 -0500

    update for new needletail version
    It was missing two methods to run SPs

--- templates/repository_template.rb
+++ templates/repository_template.rb
@@ -175,6 +175,16 @@ namespace #{@solution_name_sans_extension}.Repositories
             return this.dataSource.ExecuteScalar<T>(query: query, args: args);
         }
 
+        public void ExecuteStoredProcedure(string name, object parameters)
+        {
+            this.dataSource.ExecuteStoredProcedure(name, parameters);
+        }
+
+        public IEnumerable<T> ExecuteStoredProcedureReturnRows<T>(string name, object parameters)
+        {
+            return this.ExecuteStoredProcedureReturnRows<T>(name, parameters);
+        }
+
         public void Dispose()
         {
             this.dataSource.Dispose();
